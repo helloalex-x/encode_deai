280c049cb6c141243f753ccc216b52ae806b61a2
Alex Lopez <helloalexprb@gmail.com>
2025-03-30 19:46:33 +0200

    Initial commit from Create Next App

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,15 @@
+HEAD
 module.exports = {
   experimental: {
     appDir: false
   },
 };
+
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  /* config options here */
+};
+
+export default nextConfig;
+b273c4 (Initial commit from Create Next App)
